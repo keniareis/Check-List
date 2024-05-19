48911b58490d8d45e94cd4501d7f868b77744fbf
keniareis <keniapubg@gmail.com>
2024-05-18 21:20:20 -0300

    feat: create, update e delete methods finished

--- src/main/java/com/keniareis/CheckList/controllers/TaskController.java
+++ src/main/java/com/keniareis/CheckList/controllers/TaskController.java
@@ -1,34 +1,47 @@
 package com.keniareis.CheckList.controllers;
 
 import com.keniareis.CheckList.entities.Task;
+import com.keniareis.CheckList.exceptions.TaskNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 public class TaskController {
 
-    private ArrayList<Task> task = new ArrayList<>();
+    private ArrayList<Task> tasks = new ArrayList<>();
 
     //create
     @PostMapping
     public Task createTask(@RequestBody Task newTask){
-        Long proximoId = (long) task.size() + 1;
+        Long proximoId = (long) tasks.size() + 1;
         newTask.setId(proximoId);
-        task.add(newTask);
+        tasks.add(newTask);
         return newTask;
     }
 
     //update
     @PutMapping("/{taskid}")
-    public Optional<Task> editTask(@PathVariable UUID Id, @RequestBody Task taskUpdated) throws {
+    public Task editTask(@PathVariable Long Id, @RequestBody Task taskUpdated) throws TaskNotFoundException {
+        var newTask = tasks.stream().map(task -> {
+            if(task.getId().equals(Id))
+                taskUpdated.setId(task.getId());
+        });
+        for (Task task : tasks){
+            if (task.getId().equals(Id)){
+                task.setNome(taskUpdated.getNome());
+                task.setDate(taskUpdated.getDate());
+                task.setStatus(taskUpdated.getStatus());
+                return task;
+            }
+        }
+        throw new TaskNotFoundException("Task com o ID " + Id + " não encontrada.");
+    }
 
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        tasks.removeIf(task -> task.getId().equals(id));
     }
 
 
