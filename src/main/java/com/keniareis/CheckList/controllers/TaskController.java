64329a718ca1d27a1b175fb9bbcdd169728739cf
keniareis <keniapubg@gmail.com>
2024-06-06 09:54:52 -0300

    corrigindo o metodo GET

--- src/main/java/com/keniareis/CheckList/controllers/TaskController.java
+++ src/main/java/com/keniareis/CheckList/controllers/TaskController.java
@@ -1,9 +1,14 @@
+// TaskController.java
 package com.keniareis.CheckList.controllers;
 
 import com.keniareis.CheckList.entities.Task;
 import com.keniareis.CheckList.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,6 +19,13 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody Task task) {
         return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(@RequestParam(required = false) String sortBy) {
+        Sort sort = Sort.by(Sort.Direction.ASC, sortBy != null ? sortBy : "name");
+        return taskService.getAllTasks(sort);
+
     }
 
     @DeleteMapping("/{id}")
